5a8096f8aea62a0630b9810b7d35de931d9c944d
Moshood <moshood.yemi10@gmail.com>
2022-11-08 05:07:06 -0500

    production code cleaned

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import { verifyJWT } from "../utils/jwt";
 import { UnauthenticatedError, BadRequestError } from "../errors";
 
 const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
